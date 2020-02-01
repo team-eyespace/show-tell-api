1dc824b8ef81cfb1dc7d3805667287846235d398
abhinavtripathy <abhinavtripathy99@gmail.com>
2020-02-01 00:46:14 -0500

    setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+sudo pip3 install keras==2.2.4
+sudo pip3 install numpy==1.15.4
+sudo pip3 install nltk==3.4
+sudo pip3 install pillow
+sudo pip3 install tensorflow
+sudo pip3 install matplotlib
+pip3 install flask
+npm i
